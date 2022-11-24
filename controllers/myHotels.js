ff62a71dfe3daf0840b7096a8b6deba26b619c49
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-24 20:09:17 -0300

    mt-02-45 missing customize card

--- controllers/myHotels.js
+++ controllers/myHotels.js
@@ -1,30 +1,31 @@
 const Hotel = require('../models/Hotel')
 
 const controller = {
-    read: async(req,res) => {
-        let query = {}
+    readHU: async(req,res)=>{
+        let query = {};
         if(req.query.userId){
-            query={userId:req.query.userId}
+            query = {userId: req.query.userId};
         }
         try{
             let hotels = await Hotel.find(query)
             if(hotels){
-                res.status(200).json({
-                    response: hotels,
-                    success: true,
-                    messsage: "Hotels Founded"
-                })}else{
-                    res.status(404).json({
-                        success: false,
-                        messsage: "Hotels Not Founded"
-                    })
-                }
+            res.status(200).json({
+                response: hotels,
+                success: true,
+                message: 'Hotels founded.'
+            })}else{
+                res.status(404).json({
+                    response: [],
+                    success: false,
+                    message: 'Hotels not founded.'
+                })
+            }
         }catch(error){
             res.status(400).json({
                 success: false,
-                messsage: error.messsage
+                message: error.message
             })
-        }
+    }
 }}
 
-module.exports = controller;
+module.exports = controller
---
+++ middlewares/accountExistsSignUp.js
@@ -0,0 +1,12 @@
+const User = require("../models/User");
+const { userExistsResponse } = require("./responses");
+
+async function accountExists(req, res, next) {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return userExistsResponse(req,res);
+  }
+  return next();
+}
+
+module.exports = accountExists;
