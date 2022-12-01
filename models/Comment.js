796afcc83c01ba33bfbc12a990ee2247fb6cff70
cornicheli <gabrielcornide.pg@gmail.com>
2022-12-01 15:34:45 -0300

    fix-mt-02-57

--- models/Comment.js
+++ models/Comment.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    showId: { type: mongoose.Types.ObjectId, ref: "shows", require: true },
-    UserId: { type: mongoose.Types.ObjectId, ref: "users", require: true },
-    date: { type: Date, require: true },
-    comment: { type: String, require: true },
-});
+    showId:{type: mongoose.Types.ObjectId, ref:"shows" ,required: true},
+    userId:{type: mongoose.Types.ObjectId, ref:"users" ,required: true},
+    date: {type: Date, required: true},
+    comment:[{type: String, required: true}],
+})
+const Comment = mongoose.model('comment',schema);
 
-const Comment = mongoose.model("comment", schema);
-
-module.exports = Comment;
+module.exports = Comment
